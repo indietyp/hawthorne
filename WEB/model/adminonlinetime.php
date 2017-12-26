772fe7f1b33dcff1842ce7b39c4942c6f845e422
indietyp <bilalmahmoud@posteo.de>
2017-12-26 15:59:06 +0100

    the great purge

--- WEB/model/adminonlinetime.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    if(!HasPermission("Access admin online time page"))
-        header('Location: '.WEBSITE);
-
-?>
--- database-update.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `bp_players_online`
-	CHANGE COLUMN `connected` `connected` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `sid`,
-	CHANGE COLUMN `disconnected` `disconnected` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `connected`;
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-ALTER TABLE `bp_admin_groups` DROP COLUMN `server`;
-ALTER TABLE `bp_admins` ADD COLUMN `aid` INT(11) NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`aid`);
-ALTER TABLE `bp_admins` DROP INDEX `pid_sid`, ADD INDEX `pid` (`pid`);
-ALTER TABLE `bp_admin_groups` ADD COLUMN `isadmin` INT(1) NOT NULL DEFAULT '0' AFTER `usetime`;
