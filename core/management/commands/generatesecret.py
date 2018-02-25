fd44b4010222642c501e78ff539a3f3933863db6
indietyp <bilalmahmoud@posteo.de>
2018-02-25 19:26:43 +0100

    generatesecret added

---
+++ core/management/commands/generatesecret.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+import random
+import string
+
+
+class Command(BaseCommand):
+  help = 'Creates a new secret key'
+
+  def handle(self, *args, **options):
+    output = ''
+    for _ in range(50):
+      output += random.choice(string.printable[:-6])
+
+    self.stdout.write(self.style.WARNING('This key just got randomly generated, set the current secret key with this one below: [This is only needed once]'))
+    self.stdout.write(output)
--- secret.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-output = ''
-for _ in range(50):
-  output += random.choice(string.printable[:-6])
-
-print('This key just got randomly generated, set the current secret key with this one below: [This is only needed once]')
-print(output)
