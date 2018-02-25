1fedaf4c9c5725337484a836d116245ad545c08d
indietyp <bilalmahmoud@posteo.de>
2018-02-26 00:34:54 +0100

    wsgi fix

--- panel/wsgi.py
+++ panel/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BoomPanel.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "panel.settings")
 
 application = get_wsgi_application()
