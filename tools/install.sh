3cda240a075de0ba1e38a2dd39275d3775a937b7
indietyp <bilalmahmoud@posteo.de>
2018-03-04 02:03:33 +0100

    mkdir logs added

--- tools/install.sh
+++ tools/install.sh
@@ -95,6 +95,7 @@ main() {
   # printf "${BLUE}Configuring the project...${NORMAL}\n"
   # cp $BW/panel/local.default.py $BW/panel/local.py
   # cp $BW/supervisor.default.conf $BW/supervisor.conf
+  mkdir -p /var/log/bellwether
 
   printf "\n\n${YELLOW}Database configuration:${NORMAL}\n"
   while true; do
@@ -125,8 +126,6 @@ main() {
   sed -i "s/'PASSWORD': ''/'PASSWORD': '$dbpwd'/g" $BW/panel/local.py
 
   sed -i "s/SOCIAL_AUTH_STEAM_API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'/SOCIAL_AUTH_STEAM_API_KEY = '$stapi'/g" $BW/panel/local.py
-
-  printf "${BLUE}...${NORMAL}\n"
   sed -i "s#directory=\<replace\>#directory=$BW#g" $BW/supervisor.conf
 
   printf "${BLUE}Executing project setupcommands...${NORMAL}\n"
