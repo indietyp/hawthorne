077637a4e0d9b6553685f541bf3e0002e586d8c5
indietyp <bilalmahmoud@posteo.de>
2018-03-04 02:30:56 +0100

    better generation i think

--- core/management/commands/generatesecret.py
+++ core/management/commands/generatesecret.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     output = ''
-    for _ in range(50):
-      output += random.choice(string.printable[:-6].replace("'", '').replace('"', '').replace('/', ''))
+    for _ in range(64):
+      output += random.choice(string.ascii_letters + string.digits)
 
     self.stdout.write(self.style.WARNING('This key just got randomly generated, set the current secret key with this one below: [This is only needed once]'))
     self.stdout.write(output)
