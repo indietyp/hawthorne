fc3a19ebc474ec81f9dad42b60f5dece394a4585
indietyp <bilalmahmoud@posteo.de>
2018-03-04 01:07:05 +0100

    LOCALES, WUH

--- tools/install.sh
+++ tools/install.sh
@@ -32,8 +32,8 @@ main() {
     exit 1
   fi
 
-  locale-gen --purge en_US.UTF-8
-  echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\n' > /etc/default/locale
+  LC_ALL=en_US.UTF-8
+  LANG=en_US.UTF-8
 
   BW=/bellwether
   printf "${YELLOW}This is the automatic and guided installation. ${NORMAL}\n"
