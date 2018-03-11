09d56ef047686f7e3857a2950d94124af1a8a3d0
indietyp <bilalmahmoud@posteo.de>
2018-03-11 23:28:49 +0100

    make the make tool silent
    *badumtss*

---
+++ README.md
@@ -0,0 +1,2 @@
+# I am a README.
+I am to lazy to write one currently. So have fun. This will follow later tho.
--- tools/install.sh
+++ tools/install.sh
@@ -132,7 +132,7 @@ main() {
     wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
     tar -xzvf ruby-install-0.6.1.tar.gz
     cd ruby-install-0.6.1/
-    sudo make install
+    sudo make install --silent
     cd ..
     rm -rf ruby-install-0.6.1
     rm ruby-install-0.6.1.tar.gz
