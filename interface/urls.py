1b2e60992358dc528eeac123f997130608960d26
indietyp <bilalmahmoud@posteo.de>
2018-03-18 22:03:21 +0100

    may the home be with you

--- interface/urls.py
+++ interface/urls.py
@@ -15,8 +15,4 @@ urlpatterns = [
 
     path('login', views.login),
     path('logout', auth_views.LogoutView.as_view(next_page='/'), name='logout')
-    # path('articles/2003/', views.special_case_2003),
-    # path('articles/<int:year>/', views.year_archive),
-    # path('articles/<int:year>/<int:month>/', views.month_archive),
-    # path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail),
 ]
