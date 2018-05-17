15164cc72f43a4f6445af8a2d160fd384db2a021
indietyp <bilalmahmoud@posteo.de>
2018-05-17 14:05:18 +0200

    import cli tool

---
+++ core/management/commands/import.py
@@ -0,0 +1,58 @@
+from django.core.management.base import BaseCommand
+from lib.importer import Importer
+
+
+class Command(BaseCommand):
+  help = 'Imports from different external systems'
+
+  def add_arguments(self, parser):
+    parser.add_argument(
+      '--mode',
+      action='store',
+      dest='mode',
+      help='Currently supported: sourcemod',
+    )
+
+    parser.add_argument(
+      '--host',
+      action='store',
+      dest='host',
+      help='DB Host',
+    )
+
+    parser.add_argument(
+      '--user',
+      action='store',
+      dest='user',
+      help='DB User',
+    )
+
+    parser.add_argument(
+      '--password',
+      action='store',
+      dest='pwd',
+      help='DB Password',
+    )
+
+    parser.add_argument(
+      '--database',
+      action='store',
+      dest='db',
+      help='DB Database',
+    )
+
+
+  def handle(self, mode, host, user, pwd, db, *args, **options):
+    mode = mode.lower()
+    if mode not in ['sourcemod']:
+      self.stdout.write(self.style.WARNING('Currently your selected mode is not available'))
+      return
+
+    domain = host.split(":")[0]
+    port = int(host.split(":")[1])
+    importer = Importer(domain, port, user, pwd, db)
+
+    if mode == 'sourcemod':
+      importer.sourcemod()
+
+    self.stdout.write(self.style.SUCCESS('Successfully imported'))
--- interface/compilers/__init__.py
+++ interface/compilers/__init__.py
@@ -1,8 +1,10 @@
-from static_precompiler import exceptions, utils
-from static_precompiler.compilers import base
+import os
+from static_precompiler import exceptions, utils, url_converter
+from static_precompiler.compilers import base, less
 
 __all__ = (
   "CoyoteCompiler",
+  "ImprovedLESS"
 )
 
 
@@ -45,3 +47,52 @@ class CoyoteCompiler(base.BaseCompiler):
 
   def compile_source(self, source):
     raise exceptions.StaticCompilationError("This is currently not supported")
+
+
+class ImprovedLESS(less.LESS):
+  def compile_file(self, source_path):
+    full_source_path = self.get_full_source_path(source_path)
+    full_output_path = self.get_full_output_path(source_path)
+
+    # `cwd` is a directory containing `source_path`.
+    # Ex: source_path = '1/2/3', full_source_path = '/abc/1/2/3' -> cwd = '/abc'
+    cwd = os.path.normpath(os.path.join(full_source_path, *([".."] * len(source_path.split("/")))))
+
+    args = [
+        self.executable
+    ]
+    if self.is_sourcemap_enabled:
+        args.extend([
+            "--source-map"
+        ])
+    if self.include_path:
+        args.extend([
+            "--include-path={}".format(self.include_path)
+        ])
+    if self.clean_css:
+        args.extend([
+            "--clean-css",
+        ])
+    if self.global_vars:
+        for variable_name, variable_value in self.global_vars.items():
+            args.extend([
+                "--global-var={0}={1}".format(variable_name, variable_value),
+            ])
+
+    args.extend([
+        self.get_full_source_path(source_path),
+    ])
+    return_code, out, errors = utils.run_command(args, cwd=cwd)
+
+    with open(full_output_path, "w") as inp:
+      inp.write(out)
+
+    if return_code:
+        raise exceptions.StaticCompilationError(errors)
+
+    url_converter.convert_urls(full_output_path, source_path)
+
+    if self.is_sourcemap_enabled:
+        utils.fix_sourcemap(full_output_path + ".map", source_path, full_output_path)
+
+    return self.get_output_path(source_path)
