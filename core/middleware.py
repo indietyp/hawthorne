1ce52e9a4d0f2ba7127e4c160c900698aec0c129
Bilal Mahmoud <bilal-23@gmx.de>
2018-09-03 12:59:51 +0200

    added experimental token middleware

--- core/middleware.py
+++ core/middleware.py
@@ -1,7 +1,10 @@
 import logging
 
+from core.models import Token, User
 from django.conf import settings
-from django.utils import translation
+from django.utils import timezone, translation
+from hashids import Hashids as Hasher
+from uuid import UUID
 
 logger = logging.getLogger(__name__)
 
@@ -35,11 +38,56 @@ class LanguageMiddleware:
 
   def __call__(self, request):
     language_code = self.get_browser_language(request)
-    # print(language_code)
-    # print(request.LANGUAGE_CODE)
 
     if language_code:
       translation.activate(language_code)
 
     response = self.get_response(request)
     return response
+
+
+class TokenMiddleware:
+    def __init__(self, get_response):
+      self.get_response = get_response
+
+    def __retrieve__(self, request):
+      token = None
+      user = None
+
+      # optional user
+      if 'HTTP_X_MODIFIED_BY' in request.META:
+        user = request.META['HTTP_X_MODIFIED_BY']
+      if 'HTTP_X_TOKEN' in request.META:
+        token = request.META['HTTP_X_TOKEN']
+      else:
+        return token
+
+      if len(token) == 25:
+        hasher = Hasher(salt=settings.SECRET_KEY)
+        token = UUID(hasher.decode(token))
+
+      token = Token.objects.filter(id=token, is_active=True, is_anonymous=False)
+      if token:
+        token = token[0]
+        request.user = token.owner
+
+        user = User.objects.filter(id=user)
+        if token.has_perm('core.propagate_token') and user:
+          request.user = user[0]
+
+        if token.due and token.due < timezone.now():
+          token.is_active = False
+          token.save()
+
+          token = None
+      else:
+        token = None
+
+      return token
+
+    def __call__(self, request):
+      if request.path.startswith('/api/v1'):
+        request.token = self.__retrieve__(request)
+
+      response = self.get_response(request)
+      return response
--- panel/settings.py
+++ panel/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'core.middleware.LanguageMiddleware',
+    'core.middleware.TokenMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
