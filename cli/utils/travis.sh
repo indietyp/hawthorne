70f812edf2d0019c7f6b5b68e4dc8df909a72bc1
Bilal Mahmoud <Bilal-23@gmx.de>
2018-12-28 21:31:52 +0100

    travis changes

--- cli/utils/travis.sh
+++ cli/utils/travis.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+version=$(git describe --abbrev=0 --tags --match="v*")
+branch=$(git rev-parse --abbrev-ref HEAD)
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push indietyp/hawthorne
+docker push indietyp/hawthorne:latest
+
+if [[ branch = 'master' ]]; then
+  docker push indietyp/hawthorne:$stable
+fi
+docker push indietyp/hawthorne:$version
