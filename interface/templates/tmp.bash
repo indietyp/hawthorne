38088dc6c94a9908a774bb62add52ac0623f711b
Bilal Mahmoud <bilalmahmoud@posteo.net>
2019-03-24 02:37:00 +0100

    removed the tmp i18n prepend script

--- interface/templates/tmp.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in $(grep --files-without-match --include \*.pug -r "load i18n" .); do
-	exec 3<> $file && awk -v TEXT="- load i18n" 'BEGIN {print TEXT}{print}' $file >&3
-done
-
