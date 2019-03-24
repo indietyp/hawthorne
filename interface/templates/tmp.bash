1a83448837b971a1b7048a179b63c7e03163ab51
Bilal Mahmoud <bilalmahmoud@posteo.net>
2019-03-24 02:36:43 +0100

    I like to load my i18n.
    I definetly did not nuke all files several times

--- interface/templates/tmp.bash
+++ interface/templates/tmp.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for file in $(grep --files-without-match --include \*.pug -r "load i18n" .); do
-	ex -c '0r - load i18n|x' $file
+	exec 3<> $file && awk -v TEXT="- load i18n" 'BEGIN {print TEXT}{print}' $file >&3
 done
+
